73ffb3f4f55db951305c8d91fad2b6576976c93a
amitx <amitxd3011@gmail.com>
2024-07-23 15:44:33 +0300

    /

--- frontend/src/pages/CardsOfSetPage.js
+++ frontend/src/pages/CardsOfSetPage.js
@@ -1,16 +1,17 @@
+import React from "react";
 import { Link, useParams } from "react-router-dom";
 import { useCardsContext } from "../components/context/cardsProvider";
 import { useState, useEffect } from "react";
+import "./CardsOfSetPage.css"; // Ensure you import the CSS file
 
 function CardsOfSet() {
   const { setID, setName } = useParams();
-  const { cards, selectedSet } = useCardsContext(); // This will be the array of cards
+  const { cards, selectedSet } = useCardsContext();
   const [filteredCards, setFilteredCards] = useState([]);
-  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    if (cards && cards.length > 0) {
-      const filtered = cards.filter((card) => {
+    if (selectedSet) {
+      const filteredCards = cards.filter((card) => {
         return (
           card.card_sets &&
           card.card_sets.some((set) => {
@@ -19,24 +20,24 @@ function CardsOfSet() {
           })
         );
       });
-      setFilteredCards(filtered);
-      setLoading(false); // Data processing is complete, set loading to false
+      setFilteredCards(filteredCards);
     }
-  }, [setID, setName, cards]);
-
-  if (loading) {
-    return <h1>Loading...</h1>;
-  }
+  }, [setID, setName, cards, selectedSet]);
 
   if (filteredCards.length === 0) {
     return <h1>No cards found for this set.</h1>;
   }
 
-  console.log("selected set", selectedSet);
   return (
-    <div>
-      <h1>Cards of Set {`${setName}(${setID})`}</h1>
-      <h2>selected set: {selectedSet ? `${selectedSet.set_name}` : "Not available"}</h2>
+    <div className="page-container">
+      <h1 className="main-title">Cards of Set {`${setName}(${setID})`}</h1>
+      <h2 className="sub-title">
+        <ul>
+          <li>{selectedSet.set_name} cards</li>
+          <li>{selectedSet.set_code} cards</li>
+          <li>Released {selectedSet.tcg_date}</li>
+        </ul>
+      </h2>
       <div className="card-list">
         {filteredCards.map((card) => (
           <div key={card.id} className="card-item">
@@ -44,7 +45,6 @@ function CardsOfSet() {
               <img src={card.card_images[0].image_url_small} alt={card.name} />
               <h2>{card.name}</h2>
             </Link>
-            <p>{card.desc}</p>
           </div>
         ))}
       </div>
