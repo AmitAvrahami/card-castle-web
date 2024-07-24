7fab268aa098df94011dc7b72c8834fc34059516
royidudkman <106233840+royidudkman@users.noreply.github.com>
2024-07-24 12:53:13 +0300

    .

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,9 +2,22 @@
 const express = require('express');
 const cors = require('cors');
 const apiRoutes = require('./routes/api');
+const mongoose = require('mongoose')
 
 const app = express();
 const port = 5000;
+
+const URI = "mongodb+srv://yugioh:123sdnkuj@cluster0.nh9e91t.mongodb.net/yugioh"
+mongoose.connect(URI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log('mongoDB connected'))
+    .catch(err => console.log(err))
+
+const dataSchema = new mongoose.Schema({
+    name: String,
+    id: String
+})
+
+const Data = mongoose.model('dataTable', dataScheme)
 
 app.use(cors());
 app.use(express.json());
